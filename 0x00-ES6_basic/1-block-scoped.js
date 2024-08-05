f79d62a477b8d929d2804a8269893959f4e4f12b
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-05 22:35:14 +0200

    Fixng the code for 1, by keeping the second section and just changing the declarations

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,5 +2,10 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   const task2 = true;
 
+  if (trueOrFalse) {
+    let task = true;
+    const task2 = false;
+  }
+
   return [task, task2];
 }
