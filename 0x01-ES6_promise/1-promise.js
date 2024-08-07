6af4f51bcd1f7cb9516aef9a8cb9c01575e5dee9
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-07 15:45:01 +0200

    Removing the success = true line to e line to ensure that the code works correctly

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    const success = true;
 
-    if (true) {
-      resolve("{ status: 200", "body: 'Success' } ");
-    }
-    if (false) {
-      reject("The fake API is not working currently");
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
