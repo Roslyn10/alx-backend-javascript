df28ff91c85c94cff98079bc325ce34bba26a393
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-07 21:34:31 +0200

    Fixing the syntax for the codee

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => {
-      throw new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+    .catch(() => Error(())
+    .finally(() => console.log('Got a response from the API'));
 }
