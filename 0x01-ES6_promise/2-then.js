c30a0fc83cd0d1e97a847a985c41274b9cacf936
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-07 16:44:37 +0200

    Fixing the syntax for the code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise.then(
-      (respose) => {
-      resolve({ status: 200, body: 'Success' });
+      () => {
+        resolve({ status: 200, body: 'Success' });
       },
-      (error) => {
+      () => {
         reject(new Error());
-      }
+      },
     );
-	  console.log("Got a response from the API");
+  }).finally(() => {
+    console.log('Got a response from the API');
   });
 }
