95bfd3c388225309a9472484dd2c7772113f79d4
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-08 23:38:24 +0200

    Going back to the first code

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,13 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => {
-      throw new Error();
-    })
-    .finally(() => {
-      console.log('Got a response from the API');
-    });
+  return new Promise((resolve, reject) => {
+    promise.then(
+      (response) => {
+        resolve({ status: 200, body: 'Success' });
+      },
+      (error) => {
+        reject(new Error());
+      },
+    );
+    console.log('Got a response from the API');
+  });
 }
