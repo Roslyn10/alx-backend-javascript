e58c4830cf35f8f8b19f5018a103197218fe431e
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-08 23:18:48 +0200

    Fixed an error in the return statement that returned both false, by fixing one task from task to task2

--- 0x00-ES6_basic/1-main.js
+++ 0x00-ES6_basic/1-main.js
@@ -1,6 +1,4 @@
 import taskBlock from './1-block-scoped.js';
 
-test('returns the right values', () => {
-  expect(taskBlock(true)).toEqual([false, true]);
-  expect(taskBlock(false)).toEqual([false, true]);
-});
+console.log(taskBlock(true));
+console.log(taskBlock(false));
