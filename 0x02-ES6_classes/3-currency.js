32f719e5ff61c63ae8afd858e13799beefb368d4
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-08 14:45:06 +0200

    Fixing the code from a more traditional approach to a more modern and readable

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,32 @@
 export default class Currency {
-  constructor(code, name) {
-    this._code = code;
-    this._name = name;
-  }
-
-  getCode() {
-    return this._code;
-  }
-
-  setCode(newCode) {
-    if (typeof newCode !== 'string') {
-      throw new TypeError('Code must be a string');
+    constructor(code, name) {
+      this._code = code;
+      this._name = name;
     }
-    this._code = newCode;
-  }
-
-  getName() {
-    return this._name;
-  }
-
-  setName(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
+  
+    get code() {
+      return this._code;
     }
-    this._name = newName;
+  
+    set code(value) {
+      if (typeof value !== 'string') {
+        throw new Error('TypeError: Code must be a string');
+      }
+      this._code = value;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(value) {
+      if (typeof value !== 'string') {
+        throw new Error('TypeError: Name must be a string');
+      }
+      this._name = value;
+    }
+  
+    displayFullCurrency() {
+      return `${this._name} (${this._code})`;
+    }
   }
-
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
-}
