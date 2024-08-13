8a98a7b10c2ceacb4bdd6d66c299018b6c0e323d
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-13 12:33:36 +0200

    Fixing the case sensitivity in the code from Success to success

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,4 +1,19 @@
-import handleResponseFromAPI from "./2-then";
+import handleResponseFromAPI from './2-then';
 
 const promise = Promise.resolve();
-handleResponseFromAPI(promise);
+
+const successResponse = handleResponseFromAPI(promise);
+
+successResponse
+  .then(response => {
+    console.log(response); // Log the response to verify the output
+    if (response.body === 'success' && response.status === 200) {
+      console.log('Test passed');
+    } else {
+      console.log('Test failed');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
