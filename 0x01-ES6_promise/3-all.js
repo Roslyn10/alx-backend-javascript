c41c720d3f0b1bdd5ab2c6d83d33cd923154297d
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-13 13:01:34 +0200

    Adding the code for 7

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,7 @@
-import { uploadPhoto, createUser } from "./utils.js";
-
-export default function handleProfileSignup()
+export default function handleProfileSignup() {
+  return new Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
