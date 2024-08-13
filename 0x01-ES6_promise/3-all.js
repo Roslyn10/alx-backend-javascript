5564a8a2683c2864e12e9251e9cc8b1da585d292
Roslyn10 <roslynclewis1004@gmail.com>
2024-08-13 13:41:47 +0200

    Adding the code for 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,5 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+
 export default function handleProfileSignup() {
-  return new Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((value) => {
       console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
