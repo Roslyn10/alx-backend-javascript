43c8150bba11e67d2f6dac0da0168b2df5b4e066
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-30 21:19:06 +0200

    Fixing the code to console.log from stdout

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // Function that displays a message on the console
 const displayMessage = (message) => {
-  process.stdout.write(message);
+ console.log(message);
 }
 
 module.exports = displayMessage;
