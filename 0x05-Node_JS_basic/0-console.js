61b9344fc7ccf1154a63e3160f2a4d0ba4821896
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-30 21:14:17 +0200

    Fixing the Function that prints the string argument, but removing the new line

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // Function that displays a message on the console
 function displayMessage(message) {
-	process.stdout.write(message + '\n');
+	process.stdout.write(message);
 }
 
 module.exports = displayMessage;
