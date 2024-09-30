18124b772e7112eacbd9da0bb3015bbe3894827d
Roslyn10 <roslynclewis1004@gmail.com>
2024-09-30 22:01:26 +0200

    Exporting the app. 6

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -6,6 +6,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 }).listen(1245);
+
+module.exports = app;
