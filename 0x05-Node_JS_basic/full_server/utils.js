7ccf337f4a991493aae3fc4b32ac8c43cea1e98c
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-01 14:23:41 +0200

    Adding the Structure of  the server

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,14 @@
+// Accepts a file path as an arugument
+// Reads the database asynchronously
+// returns a promise
+const fs = require('fs').promises;
+
+function readDatabase(filePath) {
+	return fs.readDatabase(filePath, 'utf-8')
+	.then((data) => {
+		return data;
+	})
+	.catch((error) => {
+		throw new Error('Error reading the file: ${error.message}');
+	});
+}
