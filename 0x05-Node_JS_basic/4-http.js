7e1844de67442718315fe88a545adab5b236f681
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-01 16:42:43 +0200

    Trying something new

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,18 @@
 // A simple HTTP server that
 // Displays "Hello Holberton School!"
 // On port 1245, assigned to the variable app
-
 const http = require('http');
 
-const app = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
