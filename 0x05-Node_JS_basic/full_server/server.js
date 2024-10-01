cf86af7a99a364d215e2e46a80e9ee3101df0585
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-01 16:13:20 +0200

    Fixing the typo in the server file

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,11 @@
 // A small Express server
 const express = require('express');
-const router = require('./routers/index');
+const router = require('./routes/index');
 
 const app = express();
 const port = 1245;
 
-app.use('/', router;
+app.use('/', router);
 app.use('/students', router);
 app.use('/students/:major', router);
 
