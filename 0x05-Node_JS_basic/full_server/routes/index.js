4f6993929415331248e18ae7d91f3ba2c4b532c7
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-01 16:19:56 +0200

    Fixing the typo in the code

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const AppController = require('../controllers/AppController');
-const StudentsController = require('../controllers/StudentContoller');
+const StudentsController = require('../controllers/StudentsController');
 
 const router = express.Router();
 
