4c118b8e3be1709c529b7bcfa8c253c31b771b32
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-02 17:34:31 +0200

    Adding the code for the third calculator but the test cases will use chai, instead of mocha

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,6 @@
+// A test file for the calculator, but it uses chai instead of mocha
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./2-calcul_chai');
+
+describe("calculateNumber()", function () {
