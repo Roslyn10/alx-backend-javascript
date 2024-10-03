c560b4d8e8b90507d7ecf3419d62fa88d84c742f
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-03 13:25:08 +0200

    Fixing the code, by removing the multiplication

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,31 +1,26 @@
 // More in depth calculator
-function calculateNumber(a, b, operation) {
-        const a_round = Math.round(a);
-        const b_round = Math.round(b);
+function calculateNumber(operation, a, b) {
+    const a_round = Math.round(a);
+    const b_round = Math.round(b);
 
-        let result;
+    let result;
 
-        switch (operation) {
-                case 'add':
-                        result = a_round + b_round;
-                        break;
-                case 'subtract':
-                        result = a_round - b_round;
-                        break;
-                case 'multiply':
-                        result = a_round * b_round;
-                        break;
-                case 'divide':
-                        if (b_round === 0) {
-                                return 'Error';
-                        }
-                        result = a_round / b_round;
-                        break;
-                default:
-                        return 'Error: Invalid operation';
-        }
+    switch (operation) {
+        case 'SUM':
+            result = a_round + b_round;
+            break;
+        case 'SUBTRACT':
+            result = a_round - b_round;
+            break;
+        case 'DIVIDE':
+            if (b_round === 0) {
+                    return 'Error';
+            }
+            result = a_round / b_round;
+            break;
+    }
 
-        return result;
+    return result;
 }
 
 module.exports = calculateNumber;
