4e23e3e71877d4d2318e6d9bbf05c7a39dbd363a
Roslyn10 <roslynclewis1004@gmail.com>
2024-10-03 14:55:00 +0200

    Rewriting the codes

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,7 +1,6 @@
-// A function that calculaates something
-function sendPaymentRequestToApi(totalAmount, totalShipping) => {
-	totalCost = calculateNumber('SUM', totalAmount, totalShipping);
-	console.log(`The total is: ${totalCost}`);
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+    const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: ${totalCost}`);
 };
 
 module.exports = sendPaymentRequestToApi;
